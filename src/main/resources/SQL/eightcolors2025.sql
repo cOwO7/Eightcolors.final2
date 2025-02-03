9c53ce1b84e5856585148bd72a6b32bcc3242f1d
penguindisc <n84066@gmail.com>
2025-02-03 13:16:42 +0900

    mainPage 및 내정보 수정(오류 수정중)

--- src/main/resources/SQL/eightcolors2025.sql
+++ src/main/resources/SQL/eightcolors2025.sql
@@ -59,6 +59,7 @@ CREATE TABLE IF NOT EXISTS users (
                                      role VARCHAR(50) DEFAULT 'ROLE_USER'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+select * from users;
 
 -- 4. 숙소 테이블
 CREATE TABLE IF NOT EXISTS residence (
